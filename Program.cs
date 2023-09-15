cacd17cffeb26c64faf334a416bfcafa312b2488
younishamrh10 <yoha06001@utb.vaxjo.se>
2023-09-15 12:43:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace uppgift2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vilken är din favoritbok");
+            String X = Console.ReadLine();
+            Console.WriteLine("Din favortit bok är " + X);
+        }
+    }
+}
